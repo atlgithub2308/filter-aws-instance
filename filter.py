b14e35dc77fdb9917a6434c8198186f73ca45251
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-10-26 14:58:46 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -18,12 +18,8 @@ to_stop = []
 
 instances = filter(lambda i: i['State']['Name'] == 'running', relay.get(D.instances))
 for instance in instances:
-    try:
-        to_stop.append(instance['InstanceId'])
-        
-    except Exception as e:
-            print('\nEC2 instance {0} not considered for stopping because of a processing error: {1}'.format(instance['InstanceId'], e))
-
+    to_stop.append(instance['InstanceId'])
+    
 print('\nFound {} instances to stop:'.format(len(to_stop)))
 print(*[instance_id for instance_id in to_stop], sep = "\n") 
 
